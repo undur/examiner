caada48914d7de8d9f60c83cdfb2a692cda8b838
Hugi Thordarson <hugi@karlmenn.is>
2025-06-20 12:43:28 +0000

    More cleanup in display

--- src/main/java/examiner/PLElementDefinitionDetailPage.java
+++ src/main/java/examiner/PLElementDefinitionDetailPage.java
@@ -1,8 +1,14 @@
 package examiner;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map.Entry;
+
 import com.webobjects.appserver.WOComponent;
 import com.webobjects.appserver.WOContext;
 
+import ng.appserver.templating.parser.NGDeclaration;
+import ng.appserver.templating.parser.NGDeclaration.NGBindingValue;
 import ng.appserver.templating.parser.model.PBasicNode;
 
 public class PLElementDefinitionDetailPage extends WOComponent {
@@ -12,5 +18,34 @@ public class PLElementDefinitionDetailPage extends WOComponent {
 
 	public PLElementDefinitionDetailPage( WOContext context ) {
 		super( context );
+	}
+
+	public String currentBindingsString() {
+		List<String> descriptions = new ArrayList<>();
+
+		for( Entry<String, NGBindingValue> binding : currentNode.bindings().entrySet() ) {
+			StringBuilder b = new StringBuilder();
+			b.append( binding.getKey() );
+			b.append( "=" );
+
+			if( binding.getValue().isQuoted() ) {
+				b.append( "\"%s\"".formatted( binding.getValue().value() ) );
+			}
+			else {
+				b.append( "$" + binding.getValue().value() );
+			}
+
+			descriptions.add( b.toString() );
+		}
+
+		return String.join( ", ", descriptions );
+	}
+
+	/**
+	 * @return The name of the declaration, if it's not an inline declaration
+	 */
+	public String currentDeclarationName() {
+		final NGDeclaration d = currentNode.tag().declaration();
+		return d.isInline() ? null : d.name();
 	}
 }
