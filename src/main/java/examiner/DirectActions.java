72c15988a5d89e786388bd27e0b692522816acf5
Hugi Thordarson <hugi@karlmenn.is>
2025-06-21 09:11:04 +0000

    Added DirectActionDefinition

--- src/main/java/examiner/DirectActions.java
+++ src/main/java/examiner/DirectActions.java
@@ -1,5 +1,7 @@
 package examiner;
 
+import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.webobjects.appserver.WODirectAction;
@@ -9,5 +11,25 @@ public class DirectActions {
 
 	public static List<Class<? extends WOElement>> directActionClasses() {
 		return Util.locateSubclassesOf( WODirectAction.class );
+	}
+
+	public record DirectActionDefinition( Class<? extends WODirectAction> directActionClass, List<Method> directActionMethods ) {
+
+		DirectActionDefinition( Class<? extends WODirectAction> directActionClass ) {
+			this( directActionClass, actionMethods( directActionClass ) );
+		}
+	}
+
+	private static List<Method> actionMethods( Class clazz ) {
+		final List<Method> list = new ArrayList<>();
+
+		for( final Method method : clazz.getDeclaredMethods() ) {
+			// FIXME: Should we bother checking for the return type and access specifiers as well? // Hugi 2025-06-21
+			if( method.getName().endsWith( "Action" ) ) {
+				list.add( method );
+			}
+		}
+
+		return list;
 	}
 }
