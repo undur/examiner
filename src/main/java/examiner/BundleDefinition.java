16077e4e7e9949daf288e0ca2e083b95f0a9a513
Hugi Thordarson <hugi@karlmenn.is>
2025-06-26 07:10:16 +0000

    Keep track of main bundle

--- src/main/java/examiner/BundleDefinition.java
+++ src/main/java/examiner/BundleDefinition.java
@@ -2,6 +2,9 @@ package examiner;
 
 import com.webobjects.foundation.NSBundle;
 
-public record BundleDefinition( NSBundle bundle ) {
+public record BundleDefinition( NSBundle bundle, boolean isMain ) {
 
+	public BundleDefinition( NSBundle bundle ) {
+		this( bundle, bundle.equals( NSBundle.mainBundle() ) );
+	}
 }
